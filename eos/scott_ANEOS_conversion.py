53f162f6fee1750463e7f7f33366feaef5c3b461
ScottHull <scottdhull21@gmail.com>
2019-09-04 12:02:50 -0400

    added eos comments

--- eos/scott_ANEOS_conversion.py
+++ eos/scott_ANEOS_conversion.py
@@ -19,9 +19,14 @@ nu = 120  # number of the grid for the internal energy (exponential)
 # This function is to correct the original ANEOS format that does not include "E"
 # This seems to  occur when the exponent reaches -101
 def reformat(number):
+    """
+    Fixes a bug in an input file.
+    :param number:
+    :return:
+    """
     if number.find('E') == -1:
         exponent = "-101"
-        mantissa  = number.split(exponent)
+        mantissa = number.split(exponent)
         return float(mantissa[0])*10**float(exponent)
     else:
         mantissa, exponent =  number.split('E')
@@ -34,51 +39,60 @@ aneosfile =  [line.split() for line in open(inputfilename)]
 temperature = np.zeros(shape=(0, 0))
 density = np.zeros(shape=(0, 0))
 
-nt = 0
+nt = 0  # get index of last unique variable before linebreak in input file
 
+# get all unique temperatures
 for i in range(1, len(aneosfile)):
     try:
-        temperature = np.append(temperature, reformat(aneosfile[i][1]))
+        temperature = np.append(temperature, reformat(aneosfile[i][1]))  # indexing is row, column
+        print(i, reformat(aneosfile[i][1]))
     except IndexError:
-        nt = i-1
+        nt = i - 1
         break
 
+# get all unique densities
 for i in range(1, len(aneosfile), nt + 1):
-    density = np.append(density,reformat(aneosfile[i][0]))
+    density = np.append(density, reformat(aneosfile[i][0]))  # indexing is row, column
 
+# get number of unique densities
+nr = len(density)  # density grid number
 
-nr = len(density) #density grid number
-
+# create a matrix of #density X #variable
 energy = np.zeros(shape=(nr, nt)) #J/kg
 pressure = np.zeros(shape=(nr, nt)) #Pa
 soundspeed = np.zeros(shape=(nr, nt)) #m/s
 entropy = np.zeros(shape=(nr, nt)) #J/kg/K
 
+"""
+Recall that nr is the number of unique densities and nt is the number of unique samples of the variable in question.
+"""
 i = 1
 for m in range(0, nr):
     for n in range(0, nt):
         try:
+            # assign variable to row and column of corresponding matrix
             energy[m][n] = reformat(aneosfile[i][2])
             pressure[m][n] = reformat(aneosfile[i][3])
             soundspeed[m][n] = reformat(aneosfile[i][4])
             entropy[m][n] = reformat(aneosfile[i][5])
 
-        except IndexError:  # skipping a line
+        except IndexError:  # skipping a line, I think for when you get to blank lines?
             i = i + 1
             energy[m][n] = reformat(aneosfile[i][2])
             pressure[m][n] = reformat(aneosfile[i][3])
             soundspeed[m][n] = reformat(aneosfile[i][4])
             entropy[m][n] = reformat(aneosfile[i][5])
-        i = i+1
+        i = i + 1  # increase row number in input file
 
 # Taking the min and max internal energy from the original ANEOS data
 umin = np.min(energy)
 umax = np.max(energy)
 
-delta = (umax/umin)**(1.0/(nu-1))
+delta = (umax / umin)**(1.0 / (nu - 1))
 
-new_energy = np.zeros(shape=(0, 0))
-for m in range(0,nu):
+new_energy = np.zeros(shape=(0, 0))  # create a new energy array
+
+for m in range(0, nu):
     new_energy = np.append(new_energy,umin*delta**m) #exponential grid
 
 new_temperature = np.zeros(shape = (nr,nu))
@@ -89,21 +103,20 @@ new_entropy = np.zeros(shape = (nr,nu))
 
 # 1D interpolation & extrapolation (linear)
 for m in range(0, nu):
-    print(energy[m,:])
     # internal energy
-    f_temperature  =  interpolate.interp1d(energy[m,:], temperature, kind = 'linear', fill_value = 'extrapolate')
+    f_temperature = interpolate.interp1d(energy[m, :], temperature, kind='linear', fill_value = 'extrapolate')
     new_temperature[m][:] = f_temperature(new_energy)
 
     # pressure
-    f_pressure   =  interpolate.interp1d(temperature, pressure[m,:], kind = 'linear', fill_value = 'extrapolate')
+    f_pressure = interpolate.interp1d(temperature, pressure[m, :], kind='linear', fill_value = 'extrapolate')
     new_pressure[m][:] = f_pressure(new_temperature[m][:])
 
     # sound speed
-    f_soundspeed   =  interpolate.interp1d(temperature, soundspeed[m,:], kind = 'linear', fill_value = 'extrapolate')
+    f_soundspeed = interpolate.interp1d(temperature, soundspeed[m, :], kind='linear', fill_value = 'extrapolate')
     new_soundspeed[m][:] = f_soundspeed(new_temperature[m][:])
 
     # entropy
-    f_entropy   =  interpolate.interp1d(temperature, entropy[m,:], kind = 'linear', fill_value = 'extrapolate')
+    f_entropy = interpolate.interp1d(temperature, entropy[m, :], kind='linear', fill_value = 'extrapolate')
     new_entropy[m][:] = f_entropy(new_temperature[m][:])
 
 
@@ -112,7 +125,7 @@ for m in range(0, nr, int(nr/6)):
 
     ax = [0, 0, 0, 0]
 
-    fig = plt.figure(figsize = (10,6.128))
+    fig = plt.figure(figsize=(10, 6.128))
 
     ax[0] = fig.add_subplot(221)
     ax[1] = fig.add_subplot(222)
@@ -136,6 +149,8 @@ for m in range(0, nr, int(nr/6)):
 
     fig.suptitle("Density: %3.3f kg/m$^3$" %(density[m]))
     fig.savefig("Density" + str(m) + ".png")
+
+# plt.show()
 
 h = open(outputfilename,'w')
 h.write("%i %i %s \n" % (nr, nu , ':Grid numbers for density and internal energy'))
