a40b60c1dc4c24e5e7ec269071c3946eb699bc2c
ScottHull <scottdhull21@gmail.com>
2019-09-05 09:45:01 -0400

    added eos comments

--- eos/scott_ANEOS_conversion.py
+++ eos/scott_ANEOS_conversion.py
@@ -88,35 +88,44 @@ for m in range(0, nr):
 umin = np.min(energy)
 umax = np.max(energy)
 
-delta = (umax / umin)**(1.0 / (nu - 1))
+delta = (umax / umin)**(1.0 / (nu - 1))  # create exponential change for new grid
 
 new_energy = np.zeros(shape=(0, 0))  # create a new energy array
 
+"""
+Create a new grid of energies that change exponentially away from the origin.
+"""
 for m in range(0, nu):
-    new_energy = np.append(new_energy,umin*delta**m) #exponential grid
+    new_energy = np.append(new_energy,umin*delta**m)  # exponential grid
 
-new_temperature = np.zeros(shape = (nr,nu))
-new_pressure = np.zeros(shape = (nr,nu))
-new_soundspeed = np.zeros(shape = (nr,nu))
-new_entropy = np.zeros(shape = (nr,nu))
+fig_energy = plt.figure()
+ax_energy = fig_energy.add_subplot(111)
+ax_energy.plot(list(range(0, nu)), new_energy)
+ax_energy.set_title('Energy as Function of Gridspacing')
+ax_energy.grid()
+
+new_temperature = np.zeros(shape=(nr, nu))
+new_pressure = np.zeros(shape=(nr, nu))
+new_soundspeed = np.zeros(shape=(nr, nu))
+new_entropy = np.zeros(shape=(nr, nu))
 
 
 # 1D interpolation & extrapolation (linear)
 for m in range(0, nu):
-    # internal energy
-    f_temperature = interpolate.interp1d(energy[m, :], temperature, kind='linear', fill_value = 'extrapolate')
+    # approximate temperature given internal energy
+    f_temperature = interpolate.interp1d(energy[m, :], temperature, kind='linear', fill_value='extrapolate')
     new_temperature[m][:] = f_temperature(new_energy)
 
-    # pressure
-    f_pressure = interpolate.interp1d(temperature, pressure[m, :], kind='linear', fill_value = 'extrapolate')
+    # approximate pressure given temperature
+    f_pressure = interpolate.interp1d(temperature, pressure[m, :], kind='linear', fill_value='extrapolate')
     new_pressure[m][:] = f_pressure(new_temperature[m][:])
 
-    # sound speed
-    f_soundspeed = interpolate.interp1d(temperature, soundspeed[m, :], kind='linear', fill_value = 'extrapolate')
+    # approximate sound speed given temperature
+    f_soundspeed = interpolate.interp1d(temperature, soundspeed[m, :], kind='linear', fill_value='extrapolate')
     new_soundspeed[m][:] = f_soundspeed(new_temperature[m][:])
 
-    # entropy
-    f_entropy = interpolate.interp1d(temperature, entropy[m, :], kind='linear', fill_value = 'extrapolate')
+    # approximate entropy given temperature
+    f_entropy = interpolate.interp1d(temperature, entropy[m, :], kind='linear', fill_value='extrapolate')
     new_entropy[m][:] = f_entropy(new_temperature[m][:])
 
 
@@ -132,13 +141,13 @@ for m in range(0, nr, int(nr/6)):
     ax[2] = fig.add_subplot(223)
     ax[3] = fig.add_subplot(224)
 
-    ax[0].semilogy(temperature*1e-3, energy[m,:]*1e-6, '--', label  =  "original ANEOS")
-    ax[0].semilogy(new_temperature[m, :] * 1e-3, new_energy[:]*1e-6, '-.', label="modified")
-    ax[1].semilogy(temperature*1e-3, pressure[m,:]*1e-6,'--', new_temperature[m, :]*1e-3, new_pressure[m, :]*1e-6,'-.')
-    ax[2].plot(temperature*1e-3, soundspeed[m,:]*1e-3,'--', new_temperature[m, :]*1e-3, new_soundspeed[m, :]*1e-3,'-.')
-    ax[3].plot(temperature*1e-3, entropy[m,:]*1e-3,'--', new_temperature[m, :]*1e-3, new_entropy[m, :]*1e-3,'-.')
+    ax[0].semilogy(temperature * 1e-3, energy[m] * 1e-6, '--', label="original ANEOS")
+    ax[0].semilogy(new_temperature[m] * 1e-3, new_energy * 1e-6, '-.', label="modified")
+    ax[1].semilogy(temperature * 1e-3, pressure[m] * 1e-6,'--', new_temperature[m] * 1e-3, new_pressure[m] * 1e-6, '-.')
+    ax[2].plot(temperature * 1e-3, soundspeed[m] * 1e-3,'--', new_temperature[m] * 1e-3, new_soundspeed[m] * 1e-3, '-.')
+    ax[3].plot(temperature * 1e-3, entropy[m] * 1e-3,'--', new_temperature[m] * 1e-3, new_entropy[m] * 1e-3, '-.')
 
-    ax[0].legend(frameon = False)
+    ax[0].legend(frameon=False)
 
     ax[0].set_ylabel('Energy (MJ/kg)', fontsize=10)
     ax[1].set_ylabel('Pressure (MPa)', fontsize=10)
@@ -147,16 +156,21 @@ for m in range(0, nr, int(nr/6)):
     ax[2].set_xlabel('Temperature ($10^3$ K)', fontsize=10)
     ax[3].set_xlabel('Temperature ($10^3$ K)',fontsize=10)
 
+    ax[0].grid()
+    ax[1].grid()
+    ax[2].grid()
+    ax[3].grid()
+
     fig.suptitle("Density: %3.3f kg/m$^3$" %(density[m]))
-    fig.savefig("Density" + str(m) + ".png")
+    # fig.savefig("Density" + str(m) + ".png")
 
-# plt.show()
+plt.show()
 
-h = open(outputfilename,'w')
-h.write("%i %i %s \n" % (nr, nu , ':Grid numbers for density and internal energy'))
-h.write('Density (km/m3), Internal energy (kJ/kg), Temperature (K), Sound speed (m/s), Entropy (J/K/kg) \n')
-
-
-for m in range(0,nr):
-    for n in range(0,nu):
-        h.write("%15.8E %15.8E %15.8E %15.8E %15.8E \n" % (density[m], new_energy[n], new_temperature[m][n], new_soundspeed[m][n], new_entropy[m][n]))
+# h = open(outputfilename,'w')
+# h.write("%i %i %s \n" % (nr, nu , ':Grid numbers for density and internal energy'))
+# h.write('Density (km/m3), Internal energy (kJ/kg), Temperature (K), Sound speed (m/s), Entropy (J/K/kg) \n')
+#
+#
+# for m in range(0,nr):
+#     for n in range(0,nu):
+#         h.write("%15.8E %15.8E %15.8E %15.8E %15.8E \n" % (density[m], new_energy[n], new_temperature[m][n], new_soundspeed[m][n], new_entropy[m][n]))
